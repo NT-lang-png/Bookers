d8e867cfb7d18ed37288099abde60668ccce82fa
Your Name <183378925+NT-lang-png@users.noreply.github.com>
2024-10-13 09:05:43 +0000

    [add] bookers1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,36 +1,48 @@
 class BooksController < ApplicationController
-  
+
+protect_from_forgery
+
   def new
     @book = Book.new
     @books = Book.all
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books/:id'  #idに変える必要あり
-  end 
+    @book = Book.new(book_params)
+    if  @book.save
+    redirect_to book_path(@book)
+    else
+    render :new
+    end
+  end
 
   def index
     @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(:id)
-  end 
-  
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:title,:body)
   end
-  
+
 end
